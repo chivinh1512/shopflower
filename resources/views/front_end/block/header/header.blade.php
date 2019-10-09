6b5b1ec81522b38d61153630590163043def0022
chivinh <chivinhly.kul@gmail.com>
2019-10-09 14:21:26 +0700

    function chat on fb unfinish

--- resources/views/front_end/block/header/header.blade.php
+++ resources/views/front_end/block/header/header.blade.php
@@ -1,5 +1,31 @@
-<header>
 
+<header>
+    <!-- Load Facebook SDK for JavaScript -->
+    <div id="fb-root"></div>
+    <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+                xfbml            : true,
+                version          : 'v4.0'
+            });
+        };
+
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+
+    <!-- Your customer chat code -->
+    <div class="fb-customerchat"
+         attribution=setup_tool
+         page_id="112031363539575"
+         theme_color="#d696bb"
+         logged_in_greeting="Xin chào ! Chúng tôi đã sẵn sàng tư vấn hỗ trợ"
+         logged_out_greeting="Xin chào ! Chúng tôi đã sẵn sàng tư vấn hỗ trợ">
+    </div>
     <div class="row">
 
         <div class="col-2"></div>
